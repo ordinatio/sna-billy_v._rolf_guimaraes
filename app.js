9fbbb13757d301e63397a961e77c17bc8ec239bb
Emil Lögdberg <emil.logdberg@gmail.com>
2022-04-28 13:33:27 +0200

    added enemy generation

--- app.js
+++ app.js
@@ -2,31 +2,54 @@ var myGamePiece;
 var playerStats = {
   hp: 5,
   attackSpeed: 1,
-  level: 1,
+  level: 10,
 };
 
 function startGame() {
   myGameArea.start();
- 
-  background = new ComponentImage(
-    "images/grassbackground.png",
-    0,
-    100,
-    window.innerWidth,
-    400
-  );
-  ComponentText(`Hp:${playerStats.hp}| Attack Speed:${playerStats.attackSpeed}| Level:${playerStats.level    }`, "30px", 10, 50)
-  castle = new ComponentImage("images/castel.canon.png",-20,100,340, 375)
+  myGameArea.background()
+  generateEnemies()
+}
+
+function generateEnemies(){
+    let listOfEnemies = []
+    let i = 0
+    while(i < playerStats.level){
+    listOfEnemies.push(generateEnemy())
+    i++
+}
+console.log(listOfEnemies)
+}
+
+function generateEnemy(){
+    let enemy = new Object();
+enemy.image = `enemy_${Math.floor(Math.random() * 10)}.png`;
+enemy.speed = 1;
+enemy.hp = Math.floor(Math.random() * 10) + 1;
+enemy.attack = 1; 
+return enemy
 }
 
 var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = window.innerWidth;
+    this.canvas.width = 1280;
     this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.insertAdjacentElement("beforeend", this.canvas);
   },
+  background: function(){
+    new Component(window.innerWidth, window.innerHeight, "#87CEEB", 0, 0)
+    background = new ComponentImage(
+        "images/grassbackground.png",
+        0,
+        100,
+        window.innerWidth,
+        400
+      );
+      ComponentText(`Hp:${playerStats.hp}| Attack Speed:${playerStats.attackSpeed}| Level:${playerStats.level    }`, "30px", 10, 50, "black")
+      castle = new ComponentImage("images/castel.canon.png",-55,100,340, 375)
+  }
 };
 
 function Component(width, height, color, x, y) {
@@ -59,12 +82,14 @@ function ComponentImage(imageSrc, x, y, width, height) {
   );
 }
 
-function ComponentText(text, size, x, y) {
+function ComponentText(text, size, x, y, color) {
   this.text = text;
   this.size = size;
   this.font = "Helvetica";
   this.x = x;
   this.y = y;
+  this.color = color
+  ctx.fillStyle = color
 
   ctx.font = `${this.size} ${this.font}`;
   ctx.fillText(this.text, this.x, this.y);
